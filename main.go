8fbfccbce43633668f26254b4affddae6684d666
Keith Rarick <kr@xph.us>
2015-07-02 00:27:21 -0700

    license

--- main.go
+++ main.go
@@ -1,3 +1,8 @@
+// Copyright 2015 Keith Rarick.
+// Portions copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
